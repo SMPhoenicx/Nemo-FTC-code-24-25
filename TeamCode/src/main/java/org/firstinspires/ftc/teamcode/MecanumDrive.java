d38b2c1201694476bdcb65734c2705ecd1dd7283
Suman <sumanmuppavarapu@gmail.com>
2024-10-17 17:16:36 -0500

    Start numbering: V1.0

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDrive.java
@@ -64,13 +64,13 @@ public final class MecanumDrive {
 
         // drive model parameters
         public double inPerTick = .00105;
-        public double lateralInPerTick = inPerTick;
-        public double trackWidthTicks = 0;
+        public double lateralInPerTick =  0.00079;
+        public double trackWidthTicks = 11513.366570636983;
 
         // feedforward parameters (in tick units)
         public double kS = 1.012;
         public double kV = 0.00021;
-        public double kA = 0;
+        public double kA = 0.0000100;
 
         // path profile parameters (in inches)
         public double maxWheelVel = 50;
@@ -82,9 +82,9 @@ public final class MecanumDrive {
         public double maxAngAccel = Math.PI;
 
         // path controller gains
-        public double axialGain = 0.0;
-        public double lateralGain = 0.0;
-        public double headingGain = 0.0; // shared with turn
+        public double axialGain = 1.0;
+        public double lateralGain = 1.0;
+        public double headingGain = 1.0; // shared with turn
 
         public double axialVelGain = 0.0;
         public double lateralVelGain = 0.0;
