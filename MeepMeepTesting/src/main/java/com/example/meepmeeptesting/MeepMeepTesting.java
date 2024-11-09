ca51a156f8fd3c9c2329a359a56534380eb8213d
Anik Banerji <anikkbanerji@gmail.com>
2024-11-09 11:55:29 -0600

    started setting up trajectory sequences

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -1,6 +1,7 @@
 package com.example.meepmeeptesting;
 
 import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.Vector2d;
 import com.noahbres.meepmeep.MeepMeep;
 import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
 import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
@@ -9,20 +10,25 @@ public class MeepMeepTesting {
     public static void main(String[] args) {
         MeepMeep meepMeep = new MeepMeep(800);
 
+        Pose2d blueCloseStartPose = new Pose2d(14,61,Math.toRadians(270));
+        Pose2d blueFarStartPose = new Pose2d(-36,61,Math.toRadians(270));
+        Pose2d redCloseStartPose = new Pose2d(14,-61,Math.toRadians(90));
+        Pose2d redFarStartPose = new Pose2d(-36,-61,Math.toRadians(90));
+
+        Pose2d startPose=new Pose2d(-33,-62,Math.toRadians(180));
+
         RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
                 // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
-                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
-                .build();
+                .setConstraints(42, 30, 3, Math.toRadians(60), 16.05)
+                .setStartPose(startPose)
+                .followTrajectorySequence(drive ->
+                                drive.trajectorySequenceBuilder(startPose)
+                                        .splineToSplineHeading(redCloseStartPose)
+                                        .lineToConstantHeading(blueCloseStartPose)
+                                        .build());
 
         myBot.runAction(myBot.getDrive().actionBuilder(new Pose2d(0, 0, 0))
-                .lineToX(30)
-                .turn(Math.toRadians(90))
-                .lineToY(30)
-                .turn(Math.toRadians(90))
-                .lineToX(0)
-                .turn(Math.toRadians(90))
-                .lineToY(0)
-                .turn(Math.toRadians(90))
+                .splineTo(new Vector2d(10,20),Math.toRadians(180))
                 .build());
 
         meepMeep.setBackground(MeepMeep.Background.FIELD_INTO_THE_DEEP_JUICE_DARK)
