be7f417cb5bf6d38c48d5460e08b883621325926
Nobbertins <ayaandhuka@gmail.com>
2024-11-16 10:11:43 -0600

    added 4 parks, not tested

---
+++ TeamCode/src/main/java/strafeRightPark.java
@@ -0,0 +1,49 @@
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+@Autonomous
+public class strafeRightPark extends LinearOpMode {
+    private ElapsedTime runtime = new ElapsedTime();
+    private DcMotor leftFrontDrive = null;
+    private DcMotor leftBackDrive = null;
+    private DcMotor rightFrontDrive = null;
+    private DcMotor rightBackDrive = null;
+    @Override
+    public void runOpMode(){
+        // Initialize the hardware variables. Note that the strings used here must correspond
+        // to the names assigned during the robot configuration step on the DS or RC devices.
+        leftFrontDrive  = hardwareMap.get(DcMotor.class, "fl");
+        leftBackDrive  = hardwareMap.get(DcMotor.class, "bl");
+        rightFrontDrive = hardwareMap.get(DcMotor.class, "fr");
+        rightBackDrive = hardwareMap.get(DcMotor.class, "br");
+
+        //initialize motor directions
+        leftFrontDrive.setDirection(DcMotor.Direction.REVERSE);
+        leftBackDrive.setDirection(DcMotor.Direction.REVERSE);
+        rightFrontDrive.setDirection(DcMotor.Direction.FORWARD);
+        rightBackDrive.setDirection(DcMotor.Direction.FORWARD);
+        // Wait for the game to start (driver presses START)
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+
+        waitForStart();
+        runtime.reset();
+        double speed = 0.5;
+        while(opModeIsActive()) {
+            //strafe left
+            leftFrontDrive.setPower(speed);
+            rightBackDrive.setPower(speed);
+            leftBackDrive.setPower(-speed);
+            rightFrontDrive.setPower(-speed);
+            telemetry.addData("Elapsed Time", runtime.seconds());
+            telemetry.update();
+
+            // Break after 3 seconds as an example
+            if (runtime.seconds() > 1.3) {
+                break;
+            }
+        }
+    }
+}
